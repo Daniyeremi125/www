130d2074c7eb2b3cadd694e1654ea544c5041de5
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-04-16 12:44:27 -0500

    assss

---
+++ prototipo/proptotipo/prototipo1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>prototipo 11</title>
+</head>
+<body>ssspp
+    
+</body>
+</html>
